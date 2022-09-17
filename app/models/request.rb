2df3f5c40f7cc34683364f1073253da3cf6748a6
tuyennd-3173 <nguyen.dinh.tuyen@sun-asterisk.com>
2022-09-17 12:33:12 +0700

    Init StaticPages

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,6 +4,7 @@ class Request < ApplicationRecord
   has_many :books, through: :request_item
 
   enum status: {pending: 1, fulfilled: 2, rejected: 3}
+
   validates :day_begin, presence: true
   validates :day_end, presence: true, if: :end_greater_begin
 
