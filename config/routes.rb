366e2b167a6f137fe35003ac9e486f6848ae12b1
nguyen.dinh.tuyen <SUN-ASTERISK\nguyen.dinh.tuyen@b123173-pc.sun-asterisk.com>
2022-09-19 14:56:15 +0700

    Singup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
+
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+
+    resources :users, only: %i(new create show)
   end
 end
