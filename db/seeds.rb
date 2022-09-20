c25d0af0e02a9b26e72658a04992579aaadec71a
nguyen.dinh.tuyen <SUN-ASTERISK\nguyen.dinh.tuyen@b123173-pc.sun-asterisk.com>
2022-09-20 09:29:27 +0700

    View Books

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
 10.times do |n|
   Author.create!(
     name: Faker::Name.name,
-    dob: Faker::Date.between(from: '1965-01-01', to: '1999-12-31'),
+    dob: Faker::Date.between(from: "1965-01-01", to: "1999-12-31"),
     info: Faker::Quote.famous_last_words,
   )
 end
@@ -17,12 +17,16 @@ authors = Author.all
 
 3.times do |n|
   authors.each do |author|
-    author.books.create!(
+    book = author.books.create!(
     name: Faker::Book.title,
-    describe: Faker::Quote.jack_handey,
+    describe: Faker::Quote.robin,
     category: Faker::Quote.robin,
     publisher: Faker::Book.publisher,
     quantity: rand(10..50)
     )
+    book.image.attach({io: File.open(Rails.root.join("app", "assets", "images", "book_image.jpg")),
+      filename: "book_image.jpg",
+      content_type: "image/jpg"})
+    book.image.variant(resize: "450x300")
   end
 end
