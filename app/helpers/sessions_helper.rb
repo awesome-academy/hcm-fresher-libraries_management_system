2f20aacbcca1c4ef6d95542e3eba37ec71d9e79f
nguyen.dinh.tuyen <SUN-ASTERISK\nguyen.dinh.tuyen@b123173-pc.sun-asterisk.com>
2022-10-11 15:51:32 +0700

    Add gem devise

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,21 +1,4 @@
 module SessionsHelper
-  def log_in user
-    session[:user_id] = user.id
-  end
-
-  def current_user
-    @current_user ||= User.find_by id: session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
-  end
-
-  def log_out
-    session.delete :user_id
-    @current_user = nil
-  end
-
   def remove_book_from_cart
     if session[:book_ids].include? params[:book_id]
       session[:book_ids].delete(params[:book_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,21 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
 
-    get "/signup", to: "users#new"
-    post "/signup", to: "users#create"
+    devise_scope :user do
+      get "/signin", to: "devise/sessions#new"
+      post "/signin", to: "devise/sessions#create"
+      delete "/signout", to: "devise/sessions#destroy"
 
-    get "/login", to: "sessions#new"
-    post "/login", to: "sessions#create"
-    delete "/logout", to: "sessions#destroy"
+      get "/signup", to: "devise/registrations#new"
+      post "/signup", to: "devise/registrations#create"
+    end
+    devise_for :users
+
 
     delete "/remove_book_from_cart", to: "cart_books#destroy"
     delete "/remove_cart", to: "carts#destroy"
 
-    resources :users, only: %i(new create show)
+    resources :users, only: %i(show)
     resources :books, only: %i(index show)
     resources :carts, only: %i(index create destroy)
     resources :requests, only: %i(create index show)
